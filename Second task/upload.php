a6e0d6a1dea144f79bfb9533c89d127dea62df51
NjordGen <122793384+NjordGen@users.noreply.github.com>
2023-01-16 20:20:24 +0300

    Update upload.php

--- Second task/upload.php
+++ Second task/upload.php
@@ -5,4 +5,9 @@ foreach($file as $row){
   list($name, $conten)=$row;
   printf("%s %s \n", $name, $content);
 }
+
+mkdir("/home/runner/Zadacha-1/upload/", 0777);
+file_put_contents("/home/runner/Zadacha-1/upload/aaa.txt", "Привет");
+file_put_contents("/home/runner/Zadacha-1/upload/bbb.log", "Теcт");
+file_put_contents("/home/runner/Zadacha-1/upload/ccc.html", "<h1>Заголовок</h1>");
 ?>
