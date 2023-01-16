9de24c947afde85949930e66067323988ccb2e3b
NjordGen <122793384+NjordGen@users.noreply.github.com>
2023-01-16 18:48:44 +0300

    Delete upload.php

--- upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = new SplFileObject("Book1.csv");
-$file->setFlags(SplFileObject::READ_CSV);
-foreach($file as $row){
-  list($name, $conten)=$row;
-  printf("%s %s \n", $name, $content);
-}
-?>
