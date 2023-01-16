56c3b0c165297e838fe73eb5dbe65e9c70b4a447
NjordGen <122793384+NjordGen@users.noreply.github.com>
2023-01-16 19:48:00 +0300

    Second task

---
+++ upload.php
@@ -0,0 +1,13 @@
+<?php
+$file = new SplFileObject("Book1.csv");
+$file->setFlags(SplFileObject::READ_CSV);
+foreach($file as $row){
+  list($name, $conten)=$row;
+  printf("%s %s \n", $name, $content);
+}
+
+mkdir("C:\Users\User\Documents\GitHub\Tasks\upload", 0777);
+file_put_contents("C:\Users\User\Documents\GitHub\Tasks\upload\aaa.txt", "Привет");
+file_put_contents("C:\Users\User\Documents\GitHub\Tasks\upload\bbb.log", "Теcт");
+file_put_contents("C:\Users\User\Documents\GitHub\Tasks\upload\ccc.html", "<h1>Заголовок</h1>");
+?>
